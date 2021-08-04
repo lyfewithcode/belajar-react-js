a37cd8043c9e134e592b34b7c2ed2bf6250f0dec
lyfewithcode <lyfewithcode@gmail.com>
2021-08-04 21:01:05 +0700

    DetailView

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import './App.css';
 import { Component } from 'react';
-import { BrowserRouter, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
 
 function Home() {
   return <h2> Halaman Home </h2>
 }
 
 function ListView() {
-  return <h2> Semua User </h2>
+  return (
+    <div>
+      <h2> Semua User </h2>
+      <ul>
+        <Link to='user/ahmad'>Ahmad</Link> <br/>
+        <Link to='user/siska '>Siska</Link>
+      </ul>
+    </div>
+  )
+}
+
+function NoMatch() {
+  return <h2> 404, halaman tidak ditemukan </h2>
+}
+
+function DetailView({ match }) {
+  return <h2> Ini Halaman {match.params.name} </h2>
 }
 
 class App extends Component {
@@ -21,8 +37,12 @@ class App extends Component {
           </nav>
 
           <main>
-            <Route path='/' exact component={Home} />
-            <Route path='/users' exact component={ListView} />
+            <Switch>
+              <Route path='/' exact component={Home} />
+              <Route path='/users' exact component={ListView} />
+              <Route path='/user/:name' exact component={DetailView} />
+              <Route component={NoMatch} />
+            </Switch>
           </main>
         </div>
       </BrowserRouter>
